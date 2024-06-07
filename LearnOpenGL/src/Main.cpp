04e0f0ec85e018718b177487815ec8f668ed1e80
TheEliteD <dhristov06@gmail.com>
2024-06-07 22:36:21 +0300

    initial commit

---
+++ LearnOpenGL/src/Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    glfwInit();
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1600, 900, "Hello World", NULL, NULL);
+    if (!window) {
+        std::cout << "Window failed to create!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
